188be11eee6d83b7db914d5e6c8ac0f18ebb1276
Ionut_B <barborionut15@gmail.com>
2024-03-21 20:22:59 +0200

    L2 started

---
+++ Lab_2/problem_1/Program.cs
@@ -0,0 +1,17 @@
+﻿float point;
+byte a ,b ,c;
+
+a = 2;
+b = 5;
+c = 3;
+
+point = 12F;
+
+double a_modified = point + a - point;
+System.Console.WriteLine(a_modified);
+double b_modified = point + b - point;
+System.Console.WriteLine(b_modified);
+double c_modified = point + c - point;
+System.Console.WriteLine(c_modified);
+
+
